cd283911a81a91b2876ed9bab1a32e994ab26a50
Obay Al Bitar <albitarobay@gmail.com>
2023-10-31 15:19:14 +0000

    operational margin

--- models/intermediate/int_orders_margin.sql
+++ models/intermediate/int_orders_margin.sql
@@ -1,8 +1,9 @@
-SELECT orders_id
+SELECT orders_id 
     , date_date
-    , revenue
-    , quantity
-    , purchase_cost 
-    , margin 
+    , ROUND(SUM(revenue),2) AS revenue
+    , ROUND(SUM(quantity),2) as quantity
+    , ROUND(SUM(purchase_cost),2) as purchase_cost 
+    , ROUND(sum(margin),2) as margin 
 FROM {{ref('int_sales_margin')}}
-ORDER BY date_date DESC
+GROUP BY date_date, orders_id
+ORDER BY date_date DESC, orders_id DESC
