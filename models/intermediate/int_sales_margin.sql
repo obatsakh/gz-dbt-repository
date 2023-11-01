4fca3ef8782048af1e979ff7f68f594164a966df
Obay Al Bitar <albitarobay@gmail.com>
2023-11-01 14:57:40 +0000

    latest commits challenge 4

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -1,8 +1,8 @@
 WITH table_1 AS (Select *
-    , (quantity * purchase_price) AS purchase_cost 
+    , (quantity * purchase_price) AS purchase_cost
 from {{ref('stg_raw__sales')}} 
 JOIN {{ref('stg_raw__product')}} 
 USING (products_id))
 
-SELECT *, (revenue - purchase_cost) AS margin
-FROM table_1 
+SELECT *, (revenue - purchase_cost) AS margin, {{margin_percent ('revenue','purchase_cost')}} as margin_percent
+FROM table_1
